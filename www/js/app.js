12c11db99410903de4cdff2a5a13abc961ddc51f
auditoria11 <36539850+auditoria11@users.noreply.github.com>
2018-02-18 13:03:02 -0500

    Tarea ionic CRUD profesores
    TAREA Daniel verdecia, Leidy paola garcia, Martin rincon Molina.

--- www/js/app.js
+++ www/js/app.js
@@ -50,6 +50,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
+   .state('tab.profesor-nuevo', {
+      url: '/profesor-nuevo',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-profesor-nuevo.html',
+          controller: 'ProfesorNuevoCtrl'
+        }
+      }
+    })
+
   .state('tab.chats', {
       url: '/chats',
       views: {
@@ -60,7 +70,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     })
     .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+      url: '/chats/:profesor_rowid',
       views: {
         'tab-chats': {
           templateUrl: 'templates/chat-detail.html',
@@ -69,6 +79,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     })
 
+  
   .state('tab.account', {
     url: '/account',
     views: {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,24 +2,108 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function($scope, ConexionServ, $filter, $ionicPopup) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+  $scope.chats = ConexionServ.query('SELECT rowid, id, nombres, apellidos, Sexo, fecha_nac FROM profesores').then(function(result){
+   $scope.chats = result;
+
+});
+  $scope.remove = function(profesor) {
+   ConexionServ.query('DELETE FROM profesores WHERE rowid=?', [profesor.rowid]).then(function(result){
+
+    console.log(result)
+
+    $scope.chats = $filter('filter')($scope.chats , {rowid: '!' + profesor.rowid})
+   $ionicPopup.show({template: 'Profesor eliminado', title:'Eliminado', buttons:[
+          {
+            text: '<b>Aceptar</b>',
+            type: 'button-positive' ,
+ 
+          }]
+
+         });
+
+      });
+   };
+
+  })
+
+.controller('ChatDetailCtrl', function($scope, $state,  ConexionServ, $stateParams, $ionicPopup ) {
+     ConexionServ.query('SELECT * FROM profesores WHERE rowid=?' , [$stateParams.profesor_rowid]).then(function(result, text){
+        if(result.lenght >0) {
+       $scope.profesor = result[0];
+
+      }else{
+      $scope.no_hay = true;
+   }
+     }, function(res){
+      console.log("No se pudo traer el Profesor",res);
+
+     
+      })
+
+      $scope.actualizarProfesor = function(nombres, apellidos, Sexo, fecha_nac){
+         datos = [nombres, apellidos, Sexo, fecha_nac, $stateParams.profesor_rowid];
+         consulta = 'UPDATE profesores SET nombres=?, apellidos=?, Sexo=?, fecha_nac=? WHERE rowid=?';
+
+         console.log(datos);
+         
+
+         
+
+         ConexionServ.query(consulta, datos).then(function(result, txt){
+          $ionicPopup.show({template: 'Guardado', title:'Guardado', buttons:[
+          {
+            text: '<b>Aceptar</b>',
+            type: 'button-positive' ,
+
+          }]
+
+         });
+
+            $state.go('tab.chats');
+  
+
+         }, function(res){
+          console.log("no se pudo traer el Profesor", res);
+         })
+
+      }
+
+        
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+
+.controller('ProfesorNuevoCtrl', function($scope, ConexionServ, $ionicPopup, $state ) {
+  $scope.chats = {};
+
+  $scope.crearProfesor = function(nombres, apellidos, Sexo, fecha_nac){
+    consulta="INSERT INTO profesores(nombres, apellidos, Sexo,  fecha_nac) VALUES(?, ?, ?, ?)";
+    datos = [ nombres, apellidos, Sexo, fecha_nac];
+    ConexionServ.query(consulta, datos).then(function(result, txt){
+          $ionicPopup.show({template: 'Profesor creado', title:'creado', buttons:[
+          {
+            text: '<b>Aceptar</b>',
+            type: 'button-positive' ,
+
+          }]
+
+         });
+
+            $state.go('tab.chats');
+  
+
+         }, function(res){
+          console.log("no se pudo traer el Profesor", res);
+         })
+
+
+
+    }
+
 })
+
+
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
